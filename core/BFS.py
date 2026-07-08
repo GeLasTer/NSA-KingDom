6a6f4a3ee5738b6736f109cb01a7a28e5c1bc2ab
GeLasTer <tahaanari97@gmail.com>
2026-07-08 05:29:55 +0330

    create class for bfs.

--- core/BFS.py
+++ core/BFS.py
@@ -4,3 +4,15 @@ from collections import deque
 
 from core.graph import Graph
 from models.user import User
+
+
+class BFS:
+    """
+    Provides BFS-based algorithms on top of a Graph:
+        - raw traversal
+        - shortest path between two users
+        - distance from a source to all reachable users
+    """
+
+    def __init__(self, graph: Graph) -> None:
+        self._graph = graph
